76a6734f40121c5ef66eac61730fe64f29038344
Zachary Folk <zacharyfolk@gmail.com>
2020-07-27 23:05:32 -0700

    Ichabod

--- wp-content/themes/photo-child/header.php
+++ wp-content/themes/photo-child/header.php
@@ -2,15 +2,11 @@
 /**
  * Displays the header content
  *
- * @package Theme Freesia
- * @subpackage Photograph
- * @since Photograph 1.0
+ * @package photo-child
  */
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<?php
-$photograph_settings = photograph_get_theme_options(); ?>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
@@ -22,68 +18,16 @@ wp_head(); ?>
 <body <?php body_class(); ?>>
 	<?php
 	if ( function_exists( 'wp_body_open' ) ) {
-
 		wp_body_open();
-
 	} ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#site-content-contain"><?php esc_html_e('Skip to content','photograph');?></a>
 	<!-- Masthead ============================================= -->
 	<header id="masthead" class="site-header clearfix" role="banner">
-		<?php if ( get_header_image() ) : ?>
-		<a href="<?php echo esc_url(home_url('/'));?>" rel="home"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr(get_custom_header()->width);?>" height="<?php echo esc_attr(get_custom_header()->height);?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display'));?>"> </a>
-	<?php endif; ?>
-		<div class="header-wrap">
-
-			<!-- Top Header============================================= -->
-			<div class="top-header">
-
-				<!-- Main Header=============================================
-			       * removed id="sticky-header"
-					 -->
-				<div  class="clearfix">
-					<div class="main-header clearfix">
-						<?php do_action('photograph_site_branding'); ?>
-
-							<!-- Main Nav ============================================= -->
-							<?php
-							if($photograph_settings['photograph_disable_main_menu']==0){ ?>
-								<nav id="site-navigation" class="main-navigation clearfix" role="navigation" aria-label="<?php esc_attr_e('Main Menu','photograph');?>">
-
-								<button type="button" class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-									<span class="line-bar"></span>
-							  	</button>
-							  	<!-- end .menu-toggle -->
-								<?php if (has_nav_menu('primary')) {
-									$args = array(
-									'theme_location' => 'primary',
-									'container'      => '',
-									'items_wrap'     => '<ul id="primary-menu" class="menu nav-menu">%3$s</ul>',
-									); ?>
-
-									<?php wp_nav_menu($args);//extract the content from apperance-> nav menu
-									} else {// extract the content from page menu only
-									wp_page_menu(array('menu_class' => 'menu', 'items_wrap'     => '<ul id="primary-menu" class="menu nav-menu">%3$s</ul>'));
-									} ?>
-								</nav> <!-- end #site-navigation -->
-							<?php }
-							$photograph_side_menu = $photograph_settings['photograph_side_menu'];
-							$search_form = $photograph_settings['photograph_search_custom_header']; ?>
-								<div class="right-toggle">
-									<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-										<span class="line-bar"></span>
-								  	</button>
-								</div>
-					</div>
-				</div>
-			</div>
-		</div><!-- end .header-wrap -->
 		<?php
-
 			if(is_front_page() ) { ?>
 				<!-- Video and image header ============================================= -->
 				<?php photograph_video_category_sliders();
-
 		} ?>
 		<button type="button" class="scroll-down" type="button"><span><?php esc_html_e('menu','photograph');?></span><span></span><span></span></button><!-- Scroll Down Button -->
 	</header> <!-- end #masthead -->
