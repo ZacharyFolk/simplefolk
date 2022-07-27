1f6967b6e94b0b58d0dcf65418daf6c89235afed
ZacharyFolk <zacharyfolk@gmail.com>
2022-07-26 19:04:54 -0700

    :fire: Cleaning up, removing most of normalize, fixing breadcrumbs to be consistent

--- wp-content/themes/simplefolk/functions.php
+++ wp-content/themes/simplefolk/functions.php
@@ -497,6 +497,7 @@ function the_breadcrumb()
   $showCurrent = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
   $before = '<span class="current">'; // tag before the current crumb
   $after = '</span>'; // tag after the current crumb
+  $catLink = '<a href="/projects/"> Projects</a> ';
 
   global $post;
   $homeLink = get_bloginfo('url');
@@ -511,19 +512,7 @@ function the_breadcrumb()
       if ($thisCat->parent != 0) {
         echo get_category_parents($thisCat->parent, true, ' ' . $delimiter . ' ');
       }
-      echo '<a href="/projects/"> Projects</a> ' . $delimiter . $before . ' ' . single_cat_title('', false) . $after;
-    } elseif (is_search()) {
-      echo $before . 'Search results for "' . get_search_query() . '"' . $after;
-    } elseif (is_day()) {
-      echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
-      echo '<a href="' . get_month_link(get_the_time('Y'), get_the_time('m')) . '">' . get_the_time('F') . '</a> ' .
-        $delimiter . ' ';
-      echo $before . get_the_time('d') . $after;
-    } elseif (is_month()) {
-      echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
-      echo $before . get_the_time('F') . $after;
-    } elseif (is_year()) {
-      echo $before . get_the_time('Y') . $after;
+      echo $catLink  . $delimiter . $before . ' ' . single_cat_title('', false) . $after;
     } elseif (is_single() && !is_attachment()) {
       if (get_post_type() != 'post') {
         $post_type = get_post_type_object(get_post_type());
@@ -539,7 +528,7 @@ function the_breadcrumb()
         if ($showCurrent == 0) {
           $cats = preg_replace("#^(.+)\s$delimiter\s$#", "$1", $cats);
         }
-        echo $cats;
+        echo $catLink . ' ' . $delimiter . ' ' . $cats;
         if ($showCurrent == 1) {
           echo $before . get_the_title() . $after;
         }
@@ -547,15 +536,6 @@ function the_breadcrumb()
     } elseif (!is_single() && !is_page() && get_post_type() != 'post' && !is_404()) {
       $post_type = get_post_type_object(get_post_type());
       echo $before . $post_type->labels->singular_name . $after;
-    } elseif (is_attachment()) {
-      // $parent = get_post($post->post_parent);
-      // $cat = get_the_category($parent->ID);
-      // $cat = $cat[0];
-      // echo get_category_parents($cat, true, ' ' . $delimiter . ' ');
-      // echo '<a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a>';
-      // if ($showCurrent == 1) {
-      // echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
-      // }
     } elseif (is_page() && !$post->post_parent) {
       if ($showCurrent == 1) {
         echo $before . get_the_title() . $after;
@@ -564,7 +544,7 @@ function the_breadcrumb()
       $parent_id = $post->post_parent;
       $breadcrumbs = array();
       while ($parent_id) {
-        $page = get_page($parent_id);
+        $page = get_post($parent_id);
         $breadcrumbs[] = '<a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>';
         $parent_id = $page->post_parent;
       }
@@ -582,22 +562,8 @@ function the_breadcrumb()
     } elseif (is_tag()) {
       echo $before . '<a href="/tags/" title="View all of the tags"> Tags </a>' . $delimiter
         . ' ' . single_tag_title('', false) . $after;
-    } elseif (is_author()) {
-      global $author;
-      $userdata = get_userdata($author);
-      echo $before . 'Articles posted by ' . $userdata->display_name . $after;
-    } elseif (is_404()) {
-      echo $before . 'Error 404' . $after;
     }
-    if (get_query_var('paged')) {
-      if (is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author()) {
-        echo ' (';
-      }
-      echo __('Page') . ' ' . get_query_var('paged');
-      if (is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author()) {
-        echo ')';
-      }
-    }
+
     echo '</div>';
   }
 }
