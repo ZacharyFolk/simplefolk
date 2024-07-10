8a9ee998bfe7b9103da77f4556b89a515f533510
Zac <zacharyfolk@gmail.com>
2024-07-09 20:51:03 -0700

    💚 Adding photo meta to product pages, a lot of random prettier changes

---
+++ wp-content/themes/simplefolk/woocommerce/archive-product.php
@@ -0,0 +1,13 @@
+<?php
+defined('ABSPATH') || exit;
+
+get_header('shop'); ?>
+zzzzzzzzzzzzzz
+<main id="main" class="site-main" role="main">
+    <h1><?php esc_html_e('Custom Shop Page', 'simplefolk'); ?></h1>
+    <p>TEST TEST</p>
+</main>
+
+<?php
+get_footer('shop');
+?>
