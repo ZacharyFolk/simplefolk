01a0087d1a0cc2e840be165b320b7f7a3541c2ef
Zac <zacharyfolk@gmail.com>
2024-07-11 13:54:31 -0700

    💩 Test to see if WooCommerce override works on server

--- wp-content/themes/simplefolk/page-templates/home-template.php
+++ wp-content/themes/simplefolk/page-templates/home-template.php
@@ -10,35 +10,33 @@ get_header();
 ?>
 <div id="primary_full-width">
     <main id="main" class="site-main">
-        <div class="home-full-width">
-            <?php
-            if (is_active_sidebar('home-full-top')) : ?>
-            <aside class="widget-area" aria-label="<?php esc_attr_e('Home Full Top', 'simplefolk'); ?>">
-                <?php dynamic_sidebar('home-full-top'); ?>
-            </aside>
-            <?php endif; ?>
-        </div>
         <div class="home-top-container">
-
-            <div class="home-main-content">
-
+            <div class="home-full-width">
                 <?php the_content(); ?>
+
+                <?php
+                if (is_active_sidebar('home-full-top')) : ?>
+                    <aside class="widget-area" aria-label="<?php esc_attr_e('Home Full Top', 'simplefolk'); ?>">
+                        <?php dynamic_sidebar('home-full-top'); ?>
+                    </aside>
+                <?php endif; ?>
             </div>
+
             <div class="home-aside-container">
                 <div class="home-aside-1">
                     <?php
                     if (is_active_sidebar('home-1')) : ?>
-                    <aside class="widget-area" aria-label="<?php esc_attr_e('Home One', 'simplefolk'); ?>">
-                        <?php dynamic_sidebar('home-1'); ?>
-                    </aside>
+                        <aside class="widget-area" aria-label="<?php esc_attr_e('Home One', 'simplefolk'); ?>">
+                            <?php dynamic_sidebar('home-1'); ?>
+                        </aside>
                     <?php endif; ?>
                 </div>
                 <div class="home-aside-2">
                     <?php
                     if (is_active_sidebar('home-2')) : ?>
-                    <aside class="widget-area" aria-label="<?php esc_attr_e('Home Two', 'simplefolk'); ?>">
-                        <?php dynamic_sidebar('home-2'); ?>
-                    </aside>
+                        <aside class="widget-area" aria-label="<?php esc_attr_e('Home Two', 'simplefolk'); ?>">
+                            <?php dynamic_sidebar('home-2'); ?>
+                        </aside>
                     <?php endif; ?>
                 </div>
             </div>
